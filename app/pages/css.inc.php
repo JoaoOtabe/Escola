1f64ab9c97c38dd90dbdc06c2ef221a3c58df2e1
JoaoOtabe <joaootabe@gmail.com>
2018-09-12 22:25:07 -0300

    first commit

--- app/pages/css.inc.php
+++ app/pages/css.inc.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Gamja+Flower|Lato" rel="stylesheet">
 <link href="app/css/escola.css" rel="stylesheet">
+<link href="vendor/fontAwesome/css/fonteawesome.css" rel="stylesheet">
